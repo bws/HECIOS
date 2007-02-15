f45a4dcad33e9c723f649122e8fba035c74110bd
mbassil <mbassil>
2007-02-15 21:52:53 +0000

    Committing in modified cacheModule with simple functionality. Basically just recieves a message and performs appropriate lookup on it and then sends to proper dest, request goes to fs if not in cache, response goes up to appLayer if in cache.

--- src/cacheModule.cc
+++ src/cacheModule.cc
@@ -3,6 +3,8 @@
 #include <omnetpp.h>
 #include <cacheModule.h>
 #include <vector>
+#include <map>
+
 
 using namespace std;
 
@@ -27,6 +29,11 @@ class cacheModule : public cSimpleModule
 		virtual void initialize();
 		virtual void finish();
 		virtual void handleMessage(cMessage *msg);
+
+		int cacheFsIn;
+		int cacheFsOut;
+		int cacheAppIn;
+		int cacheAppOut;
 };
 
 Define_Module(cacheModule);
@@ -37,7 +44,13 @@ cacheModule::cacheModule()
 
 void cacheModule::initialize()
 {
-	//systemCache = malloc(sizeof(CacheEntry)*cacheSize);
+	//define input/output gates
+	cacheFsIn = findGate("cacheFsIn");
+	cacheFsOut = findGate("cacheFsOut");
+	cacheAppIn = findGate("cacheAppIn");
+	cacheAppOut = findGate("cacheAppOut");
+	
+	
 }
 
 void cacheModule::finish()
@@ -57,9 +70,6 @@ void cacheModule::handleMessage(cMessage *msg)
 		switch(msg->kind())
 		{
 		// cases for request handling
-		case mpiFileOpenRequest :
-			cacheProcess_mpiFileOpenRequest(msg);
-			break;
 		case mpiFileOpenRequest :
 			cacheProcess_mpiFileOpenRequest(msg);
 			break;
@@ -119,71 +129,186 @@ void cacheModule::handleMessage(cMessage *msg)
 }
 
 // messages from app/fs - request
+// in simplist cases, just send a response for now
 
 void cacheProcess_mpiFileOpenRequest( mpiFileOpenRequest *msg )
 {
+
+  // new response message to schedule
+  //mpiFileOpenResponse *m = new cMessage("mpiFileOpenResponse");
+  // cache search delay goes here
+  //int delay = 1;
+  // schedule message handling for sometime in future
+  //scheduleAt(delay, m);
+
+
+	// look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, cacheEntry->extent))
+    {
+		// create new message and set message fields
+    	mpiFileOpenResponse *m = new cMessage("mpiFileOpenResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileCloseRequest( mpiFileCloseRequest *msg )
 {
+	// look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, cacheEntry->extent))
+    {
+		// create new message and set message fields
+    	mpiFileCloseResponse *m = new cMessage("mpiFileCloseResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileDeleteRequest( mpiFileDeleteRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFileDeleteResponse *m = new cMessage("mpiFileDeleteResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileSetSizeRequest( mpiFileSetSizeRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFileSetSizeResponse *m = new cMessage("mpiFileSetSizeResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFilePreallocateRequest( mpiFilePreallocateRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFilePreallocateResponse *m = new cMessage("mpiFilePreallocateResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileGetSizeRequest( mpiFileGetSizeRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFileGetSizeResponse *m = new cMessage("mpiFileGetSizeResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileReadRequest( mpiFileReadRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFileReadResponse *m = new cMessage("mpiFileReadResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
 void cacheProcess_mpiFileWriteRequest( mpiFileWriteRequest *msg )
 {
+    // look in cache, if found, respond, if not found, sent to fs
+    if(cacheLookup(msg->address, msg->extent))
+    {
+		// create new message and set message fields
+    	mpiFileWriteResponse *m = new cMessage("mpiFileWriteResponse");
+		send(m, cacheAppOut);
+    }else
+    {
+        send(msg, cacheFsOut);
+    }
 }
 
-// messages to app/fs - response
+// messages to fs - responses
 
 void cacheProcess_mpiFileOpenResponse( mpiFileOpenResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFileCloseResponse( mpiFileCloseResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
+    
 }
 
 void cacheProcess_mpiFileDeleteResponse( mpiFileDeleteResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFileSetSizeResponse( mpiFileSetSizeResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFilePreallocateResponse( mpiFilePreallocateResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFileGetSizeResponse( mpiFileGetSizeResponse *msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFileReadResponse( mpiFileReadResponse *msg )
 {
+    // send response through
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 void cacheProcess_mpiFileWriteResponse( mpiFileWriteResponse*msg )
 {
+    // send response through 
+	cacheAdd(msg->offset, msg->extent);
+    send(msg, cacheAppOut);
 }
 
 // defualt message for unknown message type
@@ -195,4 +320,28 @@ void cacheUnknownMessage(void *msg)
 // timers from self
 void cacheProcessTimer( cMessage *msg )
 {
+}
+
+// helper function to perform cache lookup
+int cacheLookup(int address, int extent)
+{
+
+	// go through cache map and find entry
+
+}
+
+// helper function to perform cache add
+int cacheAdd(int address, int extent)
+{
+	// go through map look for closest entry
+	// if entry exists, combine with current,
+	// else, just add entry, no eviction right now
+
+}
+
+
+// Main function for testing funcitonality of requests
+int main()
+{
+
 }
