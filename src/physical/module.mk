93afc8e01203c4f35068eec96d8c00e0b764e88b
bradles <bradles>
2007-02-28 16:29:21 +0000

    Updating compute node to use the new enhanced version of StandardHost

--- src/physical/module.mk
+++ src/physical/module.mk
@@ -3,14 +3,6 @@
 #
 DIR := src/physical
 
-SIM_SRC += $(DIR)/beowulf_cluster.ned \
-	$(DIR)/bmi_tcp_client.cc \
-	$(DIR)/bmi_tcp_client.ned \
+SIM_SRC += $(DIR)/bmi_tcp_client.cc \
 	$(DIR)/bmi_tcp_server.cc \
-	$(DIR)/bmi_tcp_server.ned \
 	$(DIR)/bmi_transport.cc \
-	$(DIR)/bmi_transport.ned \
-	$(DIR)/compute_node.ned \
-	$(DIR)/daemon_process.ned \
-	$(DIR)/io_node.ned \
-	$(DIR)/job_process.ned
