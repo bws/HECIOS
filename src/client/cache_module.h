046e8c73fc74c3fa91c7fe4db93c3d3896464fa1
mbassil <mbassil>
2007-05-17 18:22:45 +0000

    Should be the last file to add. --M.B.

---
+++ src/client/cache_module.h
@@ -0,0 +1,13 @@
+// Name: 
+// File: cache_module.h
+
+//int dataCacheReplacePolicy;
+
+
+enum dataReplaceTypes
+{
+
+	DATA_CACHE_LRU_POLICY = 0,
+	DATA_CACHE_FIFO_POLICY
+
+} dataCacheReplacePolicy;
