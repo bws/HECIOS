b888e657d9d4fcf8d0959472e557267231fa9ced
bradles <bradles>
2009-05-27 14:50:18 +0000

    Aggregator pre-change checkin

--- src/client/aggregator/module.mk
+++ src/client/aggregator/module.mk
@@ -4,5 +4,7 @@
 DIR := src/client/aggregator
 
 SIM_SRC +=  $(DIR)/middleware_aggregator.cc \
-		$(DIR)/no_middleware_aggregator.cc
+		$(DIR)/data_sieving_middleware_aggregator.cc \
+		$(DIR)/no_middleware_aggregator.cc \
+		$(DIR)/view_aware_middleware_aggregator.cc
 		
